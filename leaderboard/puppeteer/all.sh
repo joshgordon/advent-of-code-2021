45bbec619ddf777cd9bee30d659071931f130fed
Josh Gordon <github@joshgordon.net>
2021-11-29 20:44:00 -0500

    Setting up on cron, last couple things.

---
+++ leaderboard/puppeteer/all.sh
@@ -0,0 +1,5 @@
+cd /homes/josh/puppeteer
+
+~/.nvm/versions/node/v12.10.0/bin/node index.js
+/usr/bin/s3cmd put output/full.png s3://visionist-advent-of-code/leaderboard2020.png  --acl-public --add-header="Cache-Control:no-cache"
+
